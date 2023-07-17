5775c22981c76b78ed595a490cfec57da229f793
Jasrel Peralta <jtperalta1@up.edu.ph>
2023-07-17 13:48:18 +0800

    feat: added minimal animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,11 @@ export default function Home() {
       <title>Jasrel Peralta</title>
       <div className="flex h-screen">
         <div className="m-auto">
-          <div className="flex flex-col items-center page-content">
+          <div className="flex flex-col items-center page-content group">
             <div className="page-title transition ease-in-out duration-100 hover:-translate-y-2 hover:scale-110">Jasrel Roby Peralta</div>
-            <div className="page-subtitle transition transform duration-100 hover:not-italic">Software Developer</div>
+            <div className="page-subtitle">Software Developer
+              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-black"></span>
+            </div>
           </div>
           <div className="flex flex-col items-center text-sm justify-center sm:flex-row sm:text-xl">
             <Link href="/about" className="button">About</Link>
