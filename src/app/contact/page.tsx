737b9f5def24869f7a553cebd373ac51ad51c8c6
Jasrel Peralta <jtperalta1@up.edu.ph>
2023-07-25 13:38:05 +0800

    fix: paddings and margins

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from 'next/link';
-import { MailFilled, GithubFilled, LinkedinFilled } from '@ant-design/icons';
+import { MailFilled, GithubFilled, LinkedinFilled, FontSizeOutlined } from '@ant-design/icons';
 
 export default function Contact() {
     return (
@@ -11,25 +11,25 @@ export default function Contact() {
             <div className="m-auto">
                 <div className="flex flex-col items-center">
                     <div className="page-title">Contacts</div>
-                <div className="flex flex-row justify-center p-8">
-                <div className="page-content">
-                    <Link className="button-link" href="mailto:jtperalta1@up.edu.ph">
-                        <MailFilled />
-                    </Link>
+                    <div className="flex flex-row items-center py-8 sm:py-20">
+                    <div className="page-content">
+                        <Link className="button-link" href="mailto:jtperalta1@up.edu.ph">
+                            <MailFilled />
+                        </Link>
+                    </div>
+                    <div className="page-content">
+                        <Link className="button-link" href="https://www.linkedin.com/in/jasrelperalta/">
+                            <LinkedinFilled />
+                        </Link>
+                    </div>
+                    <div className="page-content">
+                        <Link className="button-link" href="https://www.github.com/jasrelperalta">
+                            <GithubFilled />
+                        </Link>
+                    </div>
+                    </div>
+                    <Link className="button" href="/">Return</Link>
                 </div>
-                <div className="page-content">
-                    <Link className="button-link" href="https://www.linkedin.com/in/jasrelperalta/">
-                        <LinkedinFilled />
-                    </Link>
-                </div>
-                <div className="page-content">
-                    <Link className="button-link" href="https://www.github.com/jasrelperalta">
-                        <GithubFilled />
-                    </Link>
-                </div>
-            </div>
-            <Link className="button" href="/">Return</Link>
-            </div>
             </div>
         </div>
     </main>
