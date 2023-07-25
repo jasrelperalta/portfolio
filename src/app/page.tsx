eaaf8c0570e2edbce6cb575d00bb31b85bd5b110
Jasrel Peralta <jtperalta1@up.edu.ph>
2023-07-25 13:37:53 +0800

    feat: changed some classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 import Link from 'next/link';
+import { Megrim } from 'next/font/google';
+
+const megrim = Megrim({ subsets:['latin'], display: 'swap', weight: '400' });
 
 export default function Home() {
   return (
     <main>    
       <title>Jasrel Peralta</title>
       <div className="flex h-screen">
-        <div className="m-auto">
-          <div className="flex flex-col items-center page-content group">
-            <div className="page-title transition ease-in-out duration-100 hover:-translate-y-2 hover:scale-110">Jasrel Roby Peralta</div>
+        <div className="m-auto group">
+          <div className="flex flex-col items-center page-content ">
+            <div className="page-title">
+              <div id="home-name" className="group-hover:animate-pulse">
+                Jasrel Peralta
+              </div>
+            </div>
             <div className="page-subtitle">Software Developer
-              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-black"></span>
+              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-white"></span>
             </div>
           </div>
           <div className="flex flex-col items-center text-sm justify-center sm:flex-row sm:text-xl">
